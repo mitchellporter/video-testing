5bf85ec5ab59e5d47354de13034e4c042f660294
Mitchell Porter <mitchellporter@gmail.com>
2016-04-02 02:14:58 -0700

    combining CIFilter with custom monkey filter

--- CoreImageVideo/SimpleFilterViewController.swift
+++ CoreImageVideo/SimpleFilterViewController.swift
@@ -36,14 +36,30 @@ class SimpleFilterViewController: UIViewController {
             guard let input = CIImage(buffer: buffer)?.imageByApplyingTransform(transform) else {
                 return
             }
+            let filterImage = UIImage(named: "monkey")
+            let filterCIImage = CIImage(CGImage: (filterImage!.CGImage)!).imageByCompositingOverImage(input)
+
             let filter = CIFilter(name: "CIHueAdjust", withInputParameters: [
                 kCIInputAngleKey: self.angleForCurrentTime,
-                kCIInputImageKey: input
+                kCIInputImageKey: filterCIImage
                 ])
+            
             dispatch_sync(dispatch_get_main_queue()) {
                 self.coreImageView?.image = filter?.outputImage
             }
         }
         source?.running = true
+    }
+    
+    func imageWithImage(image: UIImage, newSize: CGSize) -> UIImage {
+        //UIGraphicsBeginImageContext(newSize);
+        // In next line, pass 0.0 to use the current device's pixel scaling factor (and thus account for Retina resolution).
+        // Pass 1.0 to force exact pixel size.
+        UIGraphicsBeginImageContextWithOptions(newSize, false, 1.0)
+        image.drawInRect(CGRectMake(0, 0, newSize.width, newSize.height))
+        
+        let newImage = UIGraphicsGetImageFromCurrentImageContext()
+        UIGraphicsEndImageContext()
+        return newImage;
     }
 }
